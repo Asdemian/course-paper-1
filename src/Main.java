7f4221f6e24e938ba3c8c769dd4ff63a63d87e7d
ASD <demaniks@yahoo.com>
2022-11-26 13:12:59 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Employee[] storage = new Employee[10];
+        storage[0] = new Employee("Прохор", "Шаляпин", "Андреевич", 1, 25348);
+        storage[1] = new Employee("Сергей", "Ломыкин", "Станиславович", 1, 15750);
+        storage[2] = new Employee("Галина", "Павлова", "Емельяновна", 2, 55358);
+        storage[3] = new Employee("Андрей", "Каменский", "Васильевич", 2, 35753);
+        storage[4] = new Employee("Илья", "Кузеев", "Дмитриевич", 2, 45781);
+        storage[5] = new Employee("Поликарп", "Демин", "Андреевич", 3, 38411);
+        storage[6] = new Employee("Иван", "Стальченков", "Кузмич", 3, 65325);
+        storage[7] = new Employee("Павел", "Шаляпин", "Андреевич", 4, 36453);
+        storage[8] = new Employee("Николай", "Потапенко", "Дмитриевич", 4, 38911);
+        storage[9] = new Employee("Ксения", "Казинская", "Васильевна", 5, 75350);
 
     }
 }
